bece8f601411f2811db681e79b9126327d99421f
james robert williams & stephen <student@epicodus.com>
2015-05-07 12:35:41 -0700

    add .all(), #save() and #==() to book.rb.All specs passing.

--- lib/book.rb
+++ lib/book.rb
@@ -6,4 +6,25 @@ class Book
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    books = []
+    returned_books = DB.exec("SELECT * FROM books ;")
+    returned_books.each() do |book|
+      id = book.fetch('id').to_i()
+      title = book.fetch('title')
+      books.push(Book.new(:title => title, :id => id))
+    end
+    books
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+  define_method(:==) do |another_book|
+    self.title().eql?(another_book.title()) && self.id().eql?(another_book.id())
+  end
+
+
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,10 +7,33 @@ describe(Book) do
       expect(test_book.title()).to(eq("Beowulf"))
     end
   end
+
   describe('#id') do
     it('returns the id of a book') do
       test_book = Book.new({:title => "Beowulf", :id => 1})
       expect(test_book.id()).to(eq(1))
     end
   end
+  describe('#save') do
+    it('saves book to the database') do
+      test_book = Book.new({ :title => 'Beowulf', :id => nil})
+      test_book.save
+      expect(Book.all()).to(eq([test_book]))
+    end
+  end
+  describe('.all') do
+    it('shows all books saved to the database, it is blank a first') do
+      expect(Book.all).to(eq([]))
+    end
+  end
+  describe('#==') do
+    it('ascribes equal to two objects are equal to each others') do
+      test_book = Book.new({:title => "Beowulf", :id => nil})
+      test_book2 = Book.new({:title => "Beowulf", :id => nil})
+      expect(test_book).to(eq(test_book2))
+     end
+   end
+
+
+
 end
