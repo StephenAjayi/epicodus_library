08bf0bfc8c588a0125147a75fa11f4e144d509b9
james robert williams & stephen <student@epicodus.com>
2015-05-07 12:15:27 -0700

    add spec helper file to store rspec information, create Database for library

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,16 @@
-require('rspec')
-require('book')
+require 'spec_helper'
 
 describe(Book) do
   describe('#title') do
     it('returns the title of a book') do
       test_book = Book.new({:title => "Beowulf", :id => nil})
       expect(test_book.title()).to(eq("Beowulf"))
+    end
+  end
+  describe('#id') do
+    it('returns the id of a book') do
+      test_book = Book.new({:title => "Beowulf", :id => 1})
+      expect(test_book.id()).to(eq(1))
     end
   end
 end
